f274f25e8776b4cfffb0994c5f75e758d10a8f09
chanbigbig <chanbigbig@163.com>
2019-01-27 18:04:25 +0800

    fix

--- config/admin.php
+++ config/admin.php
@@ -272,7 +272,7 @@ return [
     | Whether to display the environment at the footer of each page
     |
     */
-    'show_environment' => true,
+    'show_environment' => false,
 
     /*
     |--------------------------------------------------------------------------
---
+++ resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+        <!--<strong>Version</strong>&nbsp;&nbsp; {!! config('admin.version') !!}-->
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy 2019 <a href="{!! config('app.APP_URL') !!}" target="_blank">运营管理</a> .</strong>
+</footer>
