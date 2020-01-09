fbbd4c806a443dc8f2fc6fea53a9edfb9d5ef2b4
chanbigbig <chanbigbig@163.com>
2020-01-09 22:12:56 +0800

    fix

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Support\Facades\Log;
+
 class IndexController extends Controller
 {
     public function test()
     {
+        Log::info("hello-world");
         return $this->success();
     }
 
