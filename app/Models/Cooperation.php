1d4b96acd1794df0747b0ab5c60ea5086fba3789
chanbigbig <chanbigbig@163.com>
2020-02-02 21:50:29 +0800

    fix

--- app/Models/Cooperation.php
+++ app/Models/Cooperation.php
@@ -9,63 +9,63 @@ class Cooperation extends Model
     protected $connection = 'mysql';
     protected $table = 'cooperation';
     protected $guarded = ['id'];
-
-    protected $navigationId = 6;
-
-    public function getNavigationId()
-    {
-        return $this->navigationId;
-    }
-
-    /**
-     * 数据变更时，自动触发回调
-     */
-    public static function boot()
-    {
-        parent::boot();
-
-        self::created(function ($model)
-        {
-            if ($model->is_show_homepage == 1) {
-                NavigationChild::firstOrCreate([
-                    'child_table' => 'cooperation',
-                    'child_id' => $model->id
-                ], [
-                    'navigation_id' => self::getNavigationId(),
-                    'title' => $model->title,
-                ]);
-            }
-        });
-
-        //当更新时触发
-        self::updated(function ($model)
-        {
-            //关闭
-            if ($model->is_show_homepage == 0) {
-                NavigationChild::query()
-                    ->where('child_table', 'cooperation')
-                    ->where('child_id', $model->id)
-                    ->delete();
-            }
-            //开启
-            if ($model->is_show_homepage == 1) {
-                NavigationChild::firstOrCreate([
-                    'child_table' => 'cooperation',
-                    'child_id' => $model->id
-                ], [
-                    'navigation_id' => self::getNavigationId(),
-                    'title' => $model->title,
-                ]);
-            }
-
-        });
-
-        self::deleted(function ($model)
-        {
-            NavigationChild::query()
-                ->where('child_table', 'cooperation')
-                ->where('child_id', $model->id)
-                ->delete();
-        });
-    }
+//
+//    protected $navigationId = 6;
+//
+//    public function getNavigationId()
+//    {
+//        return $this->navigationId;
+//    }
+//
+//    /**
+//     * 数据变更时，自动触发回调
+//     */
+//    public static function boot()
+//    {
+//        parent::boot();
+//
+//        self::created(function ($model)
+//        {
+//            if ($model->is_show_homepage == 1) {
+//                NavigationChild::firstOrCreate([
+//                    'child_table' => 'cooperation',
+//                    'child_id' => $model->id
+//                ], [
+//                    'navigation_id' => self::getNavigationId(),
+//                    'title' => $model->title,
+//                ]);
+//            }
+//        });
+//
+//        //当更新时触发
+//        self::updated(function ($model)
+//        {
+//            //关闭
+//            if ($model->is_show_homepage == 0) {
+//                NavigationChild::query()
+//                    ->where('child_table', 'cooperation')
+//                    ->where('child_id', $model->id)
+//                    ->delete();
+//            }
+//            //开启
+//            if ($model->is_show_homepage == 1) {
+//                NavigationChild::firstOrCreate([
+//                    'child_table' => 'cooperation',
+//                    'child_id' => $model->id
+//                ], [
+//                    'navigation_id' => self::getNavigationId(),
+//                    'title' => $model->title,
+//                ]);
+//            }
+//
+//        });
+//
+//        self::deleted(function ($model)
+//        {
+//            NavigationChild::query()
+//                ->where('child_table', 'cooperation')
+//                ->where('child_id', $model->id)
+//                ->delete();
+//        });
+//    }
 }
--- app/Models/NeedKnow.php
+++ app/Models/NeedKnow.php
@@ -10,62 +10,62 @@ class NeedKnow extends Model
     protected $table = 'need_know';
     protected $guarded = ['id'];
 
-    protected $navigationId = 5;
-
-    public function getNavigationId()
-    {
-        return $this->navigationId;
-    }
-
-    /**
-     * 数据变更时，自动触发回调
-     */
-    public static function boot()
-    {
-        parent::boot();
-
-        self::created(function ($model)
-        {
-            if ($model->is_show_homepage == 1) {
-                NavigationChild::firstOrCreate([
-                    'child_table' => 'need_know',
-                    'child_id' => $model->id
-                ], [
-                    'navigation_id' => self::getNavigationId(),
-                    'title' => $model->title,
-                ]);
-            }
-        });
-
-        //当更新时触发
-        self::updated(function ($model)
-        {
-            //关闭
-            if ($model->is_show_homepage == 0) {
-                NavigationChild::query()
-                    ->where('child_table', 'need_know')
-                    ->where('child_id', $model->id)
-                    ->delete();
-            }
-            //开启
-            if ($model->is_show_homepage == 1) {
-                NavigationChild::firstOrCreate([
-                    'child_table' => 'need_know',
-                    'child_id' => $model->id
-                ], [
-                    'navigation_id' => self::getNavigationId(),
-                    'title' => $model->title,
-                ]);
-            }
-
-        });
-
-        self::deleted(function ($model)
-        {
-            NavigationChild::query()
-                ->where('child_table', 'need_know')
-                ->where('child_id', $model->id)
-                ->delete();
-        });
-    }
+//    protected $navigationId = 5;
+//
+//    public function getNavigationId()
+//    {
+//        return $this->navigationId;
+//    }
+//
+//    /**
+//     * 数据变更时，自动触发回调
+//     */
+//    public static function boot()
+//    {
+//        parent::boot();
+//
+//        self::created(function ($model)
+//        {
+//            if ($model->is_show_homepage == 1) {
+//                NavigationChild::firstOrCreate([
+//                    'child_table' => 'need_know',
+//                    'child_id' => $model->id
+//                ], [
+//                    'navigation_id' => self::getNavigationId(),
+//                    'title' => $model->title,
+//                ]);
+//            }
+//        });
+//
+//        //当更新时触发
+//        self::updated(function ($model)
+//        {
+//            //关闭
+//            if ($model->is_show_homepage == 0) {
+//                NavigationChild::query()
+//                    ->where('child_table', 'need_know')
+//                    ->where('child_id', $model->id)
+//                    ->delete();
+//            }
+//            //开启
+//            if ($model->is_show_homepage == 1) {
+//                NavigationChild::firstOrCreate([
+//                    'child_table' => 'need_know',
+//                    'child_id' => $model->id
+//                ], [
+//                    'navigation_id' => self::getNavigationId(),
+//                    'title' => $model->title,
+//                ]);
+//            }
+//
+//        });
+//
+//        self::deleted(function ($model)
+//        {
+//            NavigationChild::query()
+//                ->where('child_table', 'need_know')
+//                ->where('child_id', $model->id)
+//                ->delete();
+//        });
+//    }
 }
